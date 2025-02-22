e6d5117242f88524faf044708b7dcaac06bfbcc4
Aaron Boodoosingh <aaron.k.boodoosingh@gmail.com>
2025-02-22 07:46:48 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module programming_group.universitymanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens programming_group.universitymanagement to javafx.fxml;
+    exports programming_group.universitymanagement;
+}
